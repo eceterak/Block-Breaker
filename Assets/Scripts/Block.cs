1163c64c552ff868a7d94165e72194ec663aea48
Marek Bartula <bartula.marek@gmail.com>
2019-11-23 00:48:40 +0100

    New Project
    Lesson 77

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    // Configuration parameters
+    [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject sparklesVFX;
+    [SerializeField] Sprite[] hitSprites = null;
+
+    // Cached references
+    GameSession gameStatus;
+    Level level;
+
+    // State variables
+    [SerializeField] int timesHit;
+
+    private void Start()
+    {
+        CountBreakableBlocks();
+    }
+
+    private void CountBreakableBlocks()
+    {
+        level = FindObjectOfType<Level>();
+        gameStatus = FindObjectOfType<GameSession>();
+
+        if (tag == "Breakable")
+        {
+            level.CountBreakableBlocks();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (tag == "Breakable")
+        {
+            HandleHit();
+        }
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+
+        int maxHits = hitSprites.Length + 1;
+
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNexHitSprite();
+        }
+    }
+
+    private void ShowNexHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+
+        if (timesHit > hitSprites.Length)
+        {
+            Debug.Log("Block sprites out of index " + gameObject.name);
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+    }
+
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+ 
+        Destroy(gameObject);
+
+        level.BlockDestroyed();
+
+        gameStatus.AddToScore();
+
+        TriggerSparklesVFX();
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(sparklesVFX, transform.position, transform.rotation);
+
+        Destroy(sparkles, 1f);
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] private float screenWidth = 16f;
+    [SerializeField] private float minX = 1f;
+    [SerializeField] private float maxX = 15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePositionX = Input.mousePosition.x / Screen.width * screenWidth;
+
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+
+        paddlePosition.x = Mathf.Clamp(mousePositionX, minX, maxX);
+
+        transform.position = paddlePosition;
+    }
+}
